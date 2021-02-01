ed81b1c60a2a4882d0da3f343453ed06fcff4216
NicolasSteckel <44899523+NicolasSteckel@users.noreply.github.com>
2021-02-01 09:04:42 +0100

    Commit Version 2, Exercices 2 et 3

--- TD_vecteurs/TD_vecteurs.cpp
+++ TD_vecteurs/TD_vecteurs.cpp
@@ -20,11 +20,44 @@ int main(){
 	(vec1.scalaire(vec2)).affiche();
 	coincide(vec1,vec2); 
 
-	/*if (vec1.coincide(vec2))
-		cout << "Les vecteurs vec1 et vec2 coincident " << endl;
-	else
-		cout << "Les vecteurs v1 et vec2 ne coincident pas " << endl;*/
+	vect3d<float> vec3(1, 2, 3);
+	vect3d<float> vec4(2, 6, 7);
 	
+	vec3 = vec1 + vec2;
+	vec3.affiche();
 	
+	vec4 = vec1 * vec2;
+	vec4.affiche();
+
+	vec3 = vec1 - vec2;
+	cout << vec3;
+
+	//cout << vec3;
+
+	//Exercice 2
+
+	cout << "\n Exercice 2" << endl;
+
+	vect2d<float> vecA(8, 9);
+	vect2d<float> vecB(2, 1);
+	vect2d<float> vecC(0, 0);
+
+	vecC = vecA + vecB;
+	vecC.affiche2();
+
+	vecC = vecA - vecB;
+	vecC.affiche2();
+
+	vecC = vecA * vecB;
+	vecC.affiche2();
+
+	//Exercice 3
 	
+	tableau<vect2d<float>> tab1(3);
+	tab1[0] = vecA;
+	tab1[1] = vecB;
+
+
+
+	return 0;
 }
--- TD_vecteurs/Vecteur3D.h
+++ TD_vecteurs/Vecteur3D.h
@@ -52,6 +52,7 @@ public:
 		return x * v.x + y * v.y + z * v.z;
 	}
 	
+	
 
 	friend void coincide(vect3d vect1, vect3d vect2) {
 		if ((vect1.x == vect2.x) && (vect1.y == vect2.y) && (vect1.z == vect2.z)) {
@@ -62,13 +63,127 @@ public:
 		}
 	}
 
-	/*bool coincide(vect3d& v) {
-		return (x == v.x && y == v.y && z == v.z);
-	}*/
+	friend vect3d& operator+(vect3d vect1, vect3d vect2) {
+		vect3d resultat;
+
+		resultat.x = vect1.x + vect2.x;
+		resultat.y = vect1.y + vect2.y;
+		resultat.z = vect1.z + vect2.z;
+
+		return resultat;
+	}
+
+	friend vect3d& operator-(vect3d vect1, vect3d vect2) {
+		vect3d resultat;
+
+		resultat.x = vect1.x - vect2.x;
+		resultat.y = vect1.y - vect2.y;
+		resultat.z = vect1.z - vect2.z;
+
+		return resultat;
+	}
+
+	friend vect3d& operator*(vect3d vect1, vect3d vect2) {
+		vect3d resultat;
+
+		resultat.x = vect1.x * vect2.x;
+		resultat.y = vect1.y * vect2.y;
+		resultat.z = vect1.z * vect2.z;
+
+		return resultat;
+	}
+
+	friend ostream& operator<<(std::ostream& os, const vect3d<T> v) {
+		os << "x : " << v.x << ' '
+			<< "; y : " << v.y << ' '
+			<< "; z : " << v.z << ' ';
+		return os;
+	};
 	
-
-
 	
-
-
 };
+
+
+
+
+
+//Exercice 2
+template <class T> class vect2d : public vect3d<T> {
+	T x;
+	T y;
+public :
+	vect2d(T xa = 0, T yb = 0) {
+		x = xa;
+		y = yb;
+	}
+
+
+	vect2d(vect2d& v) {
+		x = v.x;
+		y = v.y;
+	}
+
+	friend vect2d& operator+(vect2d vect1, vect2d vect2) {
+		vect2d resultat;
+
+		resultat.x = vect1.x + vect2.x;
+		resultat.y = vect1.y + vect2.y;
+		
+		return resultat;
+	}
+
+	friend vect2d& operator-(vect2d vect1, vect2d vect2) {
+		vect2d resultat;
+
+		resultat.x = vect1.x - vect2.x;
+		resultat.y = vect1.y - vect2.y;
+		
+		return resultat;
+	}
+
+	friend vect2d& operator*(vect2d vect1, vect2d vect2) {
+		vect2d resultat;
+
+		resultat.x = vect1.x * vect2.x;
+		resultat.y = vect1.y * vect2.y;
+		
+		return resultat;
+	}
+
+	
+	friend void coincide2(vect2d vect1, vect2d vect2) {
+		if ((vect1.x == vect2.x) && (vect1.y == vect2.y)) {
+			cout << "Les deux vecteurs coincïdent" << endl;
+		}
+		else {
+			cout << "Les deux vecteurs ne coincïdent pas" << endl;
+		}
+	}
+
+	void affiche2() {
+		cout << "x : " << x << "; y : " << y << endl;
+	}
+};
+
+//Exercice 3
+
+template <class T> class tableau {
+	int m_taille;
+	T* vect;
+public:
+	tableau(int n) {
+		m_taille = n;
+		vect = new T[m_taille];
+	}
+	~tableau() {
+		delete vect;
+	}
+
+	T& operator[](int);
+};
+
+
+template <class T> T& tableau<T>::operator[] (int i) {
+	if ((i < 0) || (i > m_taille)) i = 0;
+	return vect[i];
+}
